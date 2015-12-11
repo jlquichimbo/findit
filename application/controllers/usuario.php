753e27d26f79dd62d2abd3759fc5108cfe184a1b
Robert Loaiza  <rploaiza@utpl.edu.ec>
2015-12-11 11:12:59 -0500

    Arreglo de usuario

--- application/controllers/usuario.php
+++ application/controllers/usuario.php
@@ -1,46 +1,37 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Usuario extends CI_Controller {
-
     private $res_msj = '';
-
     //primero tener un constructor
     function __construct() {
         //fundamental para que no de error, lo que hace es ejecutar el constructo del padre CI_Controller 
         parent::__construct();
         $this->load->model('usuario_model');
     }
-
     // funcion que se ejecuta x defecto al llamar el controlador
     public function index() {
         //$this->load->view('registrousuario_view');
     }
-
     public function registrar() {
         $user_cedula = $this->input->post('txtCedula');
         $user_nombres = $this->input->post('txtNombre');
         $user_apellidos = $this->input->post('txtApellido');
+        $file_name = $this->input->post('file_name');
         $user_telefono = $this->input->post('txtTelefono');
         $user_mail = $this->input->post('txtMail');
         $user_password = $this->input->post('txtPassword');
-
-        $user_usuario = $user_mail;
-
-        //Imprimimos los datos para verificar que los esta extrayendo correctamente:
-//        echo 'Datos a guardar: <br>';
-//        echo 'Cedula:' . $user_cedula . ' <br>';
-//        echo 'Nombre:' . $user_nombre . ' <br>';
-//        echo 'Apellido:' . $user_apellido . ' <br>';
-//        echo 'Direccion: ' . $user_direccion . '<br>';
-//        echo 'Telefono: ' . $user_telefono . '<br>';
-//        echo 'Mail: ' . $user_mail . '<br>';
-//        echo 'PWD: ' . $user_password . '<br>';
-//        die();
+        //$user_usuario = $user_mail;
+        
+        
         $this->db->trans_begin(); // inicio de transaccion
-
-        $nuevo_id = $this->usuario_model->save_new($user_cedula, $user_nombres, $user_apellidos, $user_usuario, $user_password, $user_mail, $user_telefono);
+        
+        
+        
+        $this->load->model('file');
+        $this->file->UploadImage('./public/img/','no es posible cargar imagen');
+        
+        $nuevo_id = $this->usuario_model->save_new($user_cedula, $user_nombres, $user_apellidos, $file_name, $user_telefono, $user_mail, $user_password);
+        
         if ($nuevo_id <= 0) {
             $this->db->trans_rollback();
             $this->res_msj .= error_msg('<br>Ha ocurrido un error al guardar el usuario en la base de datos.');
@@ -53,14 +44,16 @@ class Usuario extends CI_Controller {
             echo $this->res_msj;
 //            echo error_msg('<br>Ha ocurrido un error al guardar el paciente en la base de datos.');
         } else {
-            $this->res_msj .= success_msg('. Usuario Registrado');
-            echo $this->res_msj;
-            $this->load->model('file');
-            $user_usuario = $this->file->UploadImage('./public/img','no es posible cargar imagen :(');
+            header("Location:".base_url()."portal/vistaloguearUsuario");
+           /* $this->res_msj .= success_msg('. Usuario Registrado');
+            echo $this->res_msj;*/
+            
+            
+            
+            
             $this->db->trans_commit(); // finaliza la transaccion de begin
         }
     }
-
     public function editar_view($user_id) {
         $data['id_user'] = $user_id;
         $data['roles'] = $this->usuario_model->get_roles();
@@ -68,7 +61,6 @@ class Usuario extends CI_Controller {
         $view = $this->load->view('usuarios/edit_usuario', $data, TRUE);
         echo $view;
     }
-
     public function editar() {
         $user_id = $this->input->post('id_user');
         $user_cedula = $this->input->post('txtCedula');
@@ -78,10 +70,8 @@ class Usuario extends CI_Controller {
         $user_mail = $this->input->post('txtMail');
         $user_password = $this->input->post('txtPassword');
         $user_rol = $this->input->post('rol');
-
         //Aqui va a ir la ruta de la imagen
         $user_usuario = '';
-
         //Imprimimos los datos para verificar que los esta extrayendo correctamente:
 //        echo 'Datos a guardar: <br>';
 //        echo 'Cedula:' . $user_cedula . ' <br>';
@@ -92,7 +82,6 @@ class Usuario extends CI_Controller {
 //        echo 'PWD: ' . $user_password . '<br>';
 //        die();
         $this->db->trans_begin(); // inicio de transaccion
-
         $nuevo_id = $this->usuario_model->update($user_id, $user_cedula, $user_nombres, $user_apellidos, $user_usuario, $user_password, $user_mail, $user_telefono, $user_rol);
         if ($nuevo_id <= 0) {
             $this->db->trans_rollback();
@@ -108,12 +97,9 @@ class Usuario extends CI_Controller {
         } else {
             $this->res_msj .= success_msg('. Usuario Actualizado');
             echo $this->res_msj;
-
             $this->db->trans_commit(); // finaliza la transaccion de begin
         }
-
     }
-
     public function delete_view($id_user) {
         $data['id_user'] = $id_user;
         $data['usuario'] = $this->usuario_model->get_data_by_id($id_user);
@@ -121,5 +107,4 @@ class Usuario extends CI_Controller {
         echo $view;
     }
 }
-
 ?>
