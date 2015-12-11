05d62d11f3ddc3e9a48f868cf01379a74a4549c7
Rolando Neira <mrneira@utpl.edu.ec>
2015-12-11 14:39:23 -0500

    incorporacion del formulario

--- application/views/admin/locales.php
+++ application/views/admin/locales.php
@@ -1,3 +1,68 @@
+<div class="row formContEstablecer">  
+    <div class="col-sm-12">
+        <form id="form_horario">
+            <h2>Establecer Horario:</h2>
+            <hr>
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row nomLocal">
+                        <div class="col-sm-2 etiquetaHora ">
+                            <h4><b>Nombre local:</b></h4>
+                        </div>                
+                        <div class="col-sm-4">
+                            <input type="text" id="nombreLocal" class="form-control" name="nombreLocal" value="tururu" disabled>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-md-5 etiquetaHora">
+                                    <h4><b>Hora de apertura:</b></h4>
+                                </div>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" id="horaInicio" name="horaInicio" value="00 : 00" autofocus onclick="iniciarReloj()">                                
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="col-sm-12" id="relojHoraInicio"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-sm-4 etiquetaHora">
+                                    <h4><b>Hora de cierre:</b></h4>
+                                </div>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" id="horaCierre" name="horaCierre" class="form-control" value="00 : 01" onclick="openReloj2()" disabled>
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="col-sm-12" id="relojHoraCierre"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <button type="submit" class="btn btn-success btn-lg" id="ajaxformbtn" data-target="messages_div">
+                                <span class="glyphicon glyphicon-fire"></span>
+                                Establecer
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    
+                </div>
+            </div>          
+        </form>
+    </div>
+</div>
 <br>
 <div class="well">  
     <br>
@@ -37,14 +102,15 @@
                 echo tagcontent('td', $empresa->nombre_admin);
                 echo tagcontent('td', $empresa->latitud);
                 echo tagcontent('td', $empresa->longitud);
-                echo tagcontent('td', "<div id='btn".$contBtn."'></div>");
-                if($empresa->disponible==1){                    
-                    $estados[$contBtn]=1;
-                }
-                else{
-                    $estados[$contBtn]=0;
-                }
-                $contBtn=$contBtn+1;
+                echo tagcontent('td', "<div id='btn".$empresa->id."'></div>");
+                        $estados[$contBtn][0]=$empresa->id;
+                        if($empresa->disponible==1){                    
+                            $estados[$contBtn][1]=1;
+                        }
+                        else{
+                            $estados[$contBtn][1]=0;
+                        }
+                        $contBtn=$contBtn+1;
                 echo tagcontent('td', $empresa->hora_apertura);
                 echo tagcontent('td', $empresa->hora_cierre);
                 echo tagcontent('td', $editar);
@@ -59,8 +125,8 @@
 <!--<div class='onoffswitch'><input type='checkbox' name='onoffswitch12345' class='onoffswitch-checkbox' id='myonoffswitch12345'><label class='onoffswitch-label' for='myonoffswitch12345'><span class='onoffswitch-inner'></span><span class='onoffswitch-switch'></span></label></div>-->
 <script src='<?php echo base_url(); ?>complementos/js/admin.js'></script>
 <script>
-    var estados=<?php echo json_encode($estados);?>;
-    crearBoton(estados);
+    var estad=<?php echo json_encode($estados);?>;
+    crearBoton(estad);
 </script>
 <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
