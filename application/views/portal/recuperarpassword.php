8c9e5ddfc7eb04b92ec1d2c7c52ce8b1401892ab
Rolando Neira <mrneira@utpl.edu.ec>
2015-12-25 19:44:47 -0500

    Generador de password para enviar por cooreo al usuario

--- application/views/portal/recuperarpassword.php
+++ application/views/portal/recuperarpassword.php
@@ -1,3 +1,4 @@
+<br>
 <div id="form_registro" class="container well">
     <div class="row">
         <div class="col-xs-12">
@@ -5,13 +6,19 @@
         </div>
         <br/><br/>
     </div>
-    <form class="form-horizontal" action="<?=base_url("portal/enviarEmail")?>" method="post" enctype="multipart/form-data">
-         <div class="form-group">
+    <form class="form-horizontal" action="<?= base_url("portal/enviarEmail") ?>" method="post" enctype="multipart/form-data">
+        <div class="form-group">
             <label for="formGroup" class="col-sm-2 control-label">Correo:</label>
             <div class="col-sm-4">
                 <input type="email" id="formGroup" name="txtMail" class="form-control" required autofocus>
             </div>
+            <div class="col-sm-5">
+                <span class="help-block" id="msmRecuperarPass">
+                    <?php echo $mensajeEstado; ?>
+                </span>
+            </div>
         </div>
+
         <div class="form-group">
             <label for="formGroup" class="col-sm-2 control-label"></label>
             <div class="col-sm-4">
