c9b906ffb9130254c495b5a48f45d15d6f416e2c
Robert Loaiza  <rploaiza@utpl.edu.ec>
2016-01-31 01:54:08 -0500

    fin de proyecto

--- application/views/portal/static/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer navbar-fixed-bottom piePagina">
-        <a><img src="<?php echo base_url(); ?>/complementos/img/footer.png" alt="Loguito"></a><br>
-        <small><strong>email: </strong>find-it@gmail.com</small>
-    </div>
-</footer>
-</body>
-</html>
